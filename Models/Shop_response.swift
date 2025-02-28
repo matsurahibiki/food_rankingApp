4e29c163e8827a525dd2f0fe33c880b4d1f56ec9
matsura <k20108kk@aitech.ac.jp>
2025-02-28 21:21:19 +0900

    update: APIのためのモジュールをインポート

---
+++ Models/Shop_response.swift
@@ -0,0 +1,25 @@
+//
+//  ShopReaponse.swift
+//  Food_ranking
+//
+//  Created by k20108kk on 2025/02/28.
+//
+
+import Foundation
+
+struct Shop_response: Codable {
+//    var id: String
+//    var name: String
+//    var genre: String
+//    var address: String
+//    var url: String
+//    // 画像
+//
+//    init(id: String, name: String = "", genre: String = "その他", address: String = "", url: String = "") {
+//        self.id = id
+//        self.name = name
+//        self.genre = genre
+//        self.address = address
+//        self.url = url
+//    }
+}
